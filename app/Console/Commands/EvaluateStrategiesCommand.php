b33e4257058fdb6334f0848d769f5dfd584e397f
Tom Brown <tombrown86@gmail.com>
2018-02-13 18:45:05 +0000

    eval stats bugfix.. incorrect buy signal

--- app/Console/Commands/EvaluateStrategiesCommand.php
+++ app/Console/Commands/EvaluateStrategiesCommand.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Bowhead\Console\Commands;
 
 use Bowhead\Console\Kernel;
@@ -19,16 +18,15 @@ use AndreasGlaser\PPC\PPC; // https://github.com/andreas-glaser/poloniex-php-cli
  * Class ExampleCommand
  * @package Bowhead\Console\Commands
  */
-
-ini_set('memory_limit','1G'); 
+ini_set('memory_limit', '1G');
 
 class EvaluateStrategiesCommand extends Command {
 
 	use Signals,
-	 Strategies,
-	 CandleMap,
-	 OHLC,
-	 Pivots; // add our traits
+		Strategies,
+		CandleMap,
+		OHLC,
+		Pivots; // add our traits
 
 	/**
 	 * The console command name.
@@ -72,6 +70,18 @@ class EvaluateStrategiesCommand extends Command {
 
 		$strategy_open_position = [];
 
+		$list_indicators = array('adx', 'aroonosc', 'cmo', 'sar', 'cci', 'mfi', 'obv', 'stoch', 'rsi', 'macd', 'bollingerBands', 'atr', 'er', 'hli', 'ultosc', 'willr', 'roc', 'stochrsi');
+		$list_signals = ['rsi', 'stoch', 'stochrsi', 'macd', 'adx', 'willr', 'cci', 'atr', 'hli', 'ultosc', 'roc', 'er'];
+
+
+		$doubles = [];
+		$this->combinations($list_indicators, 2, $doubles);
+		$doubles = $this->unique_combinations($doubles);
+		$triplets = [];
+		$this->combinations($list_indicators, 3, $triplets);
+		$triplets = $this->unique_combinations($triplets);
+		$indicator_combinations = array_merge($doubles, $triplets);
+
 
 #		for ($take = 150; $take <= 150; $take+=25) {
 		$skipped = 0;
@@ -85,7 +95,9 @@ class EvaluateStrategiesCommand extends Command {
 				}
 			}
 			foreach (['1m', '5m', '15m', /* '30m', '1h' */] as $interval) {
+				$win_or_lose_short = $win_or_lose_long = []; //keep results here as we have multiple strats to check 
 				$data = $this->getRecentData($instrument, 200, false, date('H'), $interval, false, $min, false);
+
 				if (count($data['periods']) < 200) {
 					$skipped ++;
 					echo "\n !!!!!!!!!!!!!!! Less than 200 periods returned ($min)! \n";
@@ -98,36 +110,18 @@ class EvaluateStrategiesCommand extends Command {
 				}
 				$current_price = ($data['high'][count($data['low']) - 1] + $data['low'][count($data['low']) - 1]) / 2;
 
-				$list_indicators = array('adx', 'aroonosc', 'cmo', 'sar', 'cci', 'mfi', 'obv', 'stoch', 'rsi', 'macd', 'bollingerBands', 'atr', 'er', 'hli', 'ultosc', 'willr', 'roc', 'stochrsi');
-				$list_signals = ['rsi', 'stoch', 'stochrsi', 'macd', 'adx', 'willr', 'cci', 'atr', 'hli', 'ultosc', 'roc', 'er'];
-
-				
-				$doubles = [];
-				$this->combinations($list_indicators, 2, $doubles);
-				$doubles = $this->unique_combinations($doubles);
-				$triplets = [];
-				$this->combinations($list_indicators, 3, $triplets);
-				$triplets = $this->unique_combinations($triplets);
-				$indicator_combinations = array_merge($doubles, $triplets);
-				
-				
-				$instruments = ['EUR/USD'];
-
 				// candles
 				$candles = $cand->allCandles('EUR/USD', $data);
 
 				// signals
 //				$signals = $this->signals(1, 0, ['EUR/USD'], $data);
-
 				// trends
-
 //				foreach ($instruments as $instrument) {
 //					$trends[$instrument]['httc'] = $ind->httc($instrument, $data);   # Hilbert Transform - Trend vs Cycle Mode
 //					$trends[$instrument]['htl'] = $ind->htl($instrument, $data); # Hilbert Transform - Instantaneous Trendline
 //					$trends[$instrument]['hts'] = $ind->hts($instrument, $data, true); # Hilbert Transform - Sinewave
 //					$trends[$instrument]['mmi'] = $ind->mmi($instrument, $data); # market meanness
 //				}
-
 				// our indicators
 				$indicator_results = $ind->allSignals('EUR/USD', $data);
 
@@ -138,62 +132,55 @@ class EvaluateStrategiesCommand extends Command {
 				foreach ($indicator_combinations as $indicators) {
 					$strategy_name = '';
 					foreach ($indicators as $indicator_name) {
-						$strategy_name .= '_'.$indicator_name;
+						$strategy_name .= '_' . $indicator_name;
 					}
 
 					if (in_array($strategy_name, $strategy_open_position)) {
 						continue;
 					}
 
-
-					$all_overbought = $all_underbought = TRUE;
-
-					foreach($indicators as $indicator) {
-						if(!isset($indicator_results[$indicator])) {
-							continue;
+					$indicators_overbought = $indicators_underbought = TRUE;
+					foreach ($indicators as $indicator) {
+						if (!isset($indicator_results[$indicator])) {
+							$indicators_overbought = $indicators_underbought = FALSE;
+							break;
 						}
 
-						$all_overbought = $all_overbought && $indicator_results[$indicator] < 1;
-						$all_underbought = $all_underbought && $indicator_results[$indicator] > 1;
+						$indicators_overbought = $indicators_overbought && $indicator_results[$indicator] < 0;
+						$indicators_underbought = $indicators_underbought && $indicator_results[$indicator] > 0;
 					}
-
-					if(!$all_overbought && !$all_underbought) {
-						continue;
-					}
-
-					
-					if (isset($candles['current'])) { // check for at least 1 candle
+					if (($indicators_overbought XOR $indicators_underbought) && isset($candles['current'])) { // check for at least 1 candle
 						foreach ($candles['current'] as $candle_name => $candle_value) {
-							if ($candle_value > 0 && $all_underbought) {
+							$underbought = $overbought = 0;
+							if ($candle_value > 0 && $indicators_underbought) {
 								$underbought = 1;
-							}
-							if ($candle_value < 0 && $all_overbought) {
+							} else if ($candle_value < 0 && $indicators_overbought) {
 								$overbought = 1;
 							}
 
-							if ($overbought || $underbought) {
-								foreach ([/*'demark', 'fib_r2s2', 'fib_r3s3', */'perc_20_20', /*'perc_10_20'*/] as $bounds_method) {
+							if ($overbought XOR $underbought) {
+								foreach ([/* 'demark', 'fib_r2s2', 'fib_r3s3', */'perc_20_20', /* 'perc_10_20' */] as $bounds_method) {
 									$bounds_strategy_name = "$bounds_method $strategy_name";
-
 									if ($overbought) {
-										$long = false;
+										$long = FALSE;
 									} else if ($underbought) {
-										$long = true;
+										$long = TRUE;
 									}
+
 									$endmin = $min + (2 * 60 * 60);
-									$leverage = '222';
+									$leverage = 222;
 									list($stop, $take) = $this->get_bounds($bounds_method, $data, $long, $current_price, $leverage);
 
 									if ($long) {
-//										if (!$win_or_lose_long) {
-											$win_or_lose_long = $this->getWinOrLoose('EUR/USD', $min, $endmin, TRUE, $take, $stop);
-//										}
-										$result = $win_or_lose_long;
+										if (!isset($win_or_lose_long[$bounds_method])) {
+											$win_or_lose_long[$bounds_method] = $this->getWinOrLoose('EUR/USD', $min, $endmin, TRUE, $take, $stop);
+										}
+										$result = $win_or_lose_long[$bounds_method];
 									} else {
-//										if (!$win_or_lose_short) {
-											$win_or_lose_short = $this->getWinOrLoose('EUR/USD', $min, $endmin, FALSE, $take, $stop);
-//										}
-										$result = $win_or_lose_short;
+										if (!isset($win_or_lose_short[$bounds_method])) {
+											$win_or_lose_short[$bounds_method] = $this->getWinOrLoose('EUR/USD', $min, $endmin, FALSE, $take, $stop);
+										}
+										$result = $win_or_lose_short[$bounds_method];
 									}
 
 									// keep note of end time for this trade.
@@ -214,33 +201,33 @@ class EvaluateStrategiesCommand extends Command {
 												'total_wins' => 0,
 												'total_loses' => 0,
 												'avg_stop_take_range' => [],
-													/* these will be created as necessary
-													  'long_correct_candle' => [],
-													  'long_correct_inverse_candle' => [],
-													  'long_wrong_candle' => [],
-													  'long_wrong_inverse_candle' => [],
-													  'short_correct_candle' => [],
-													  'short_correct_inverse_candle' => [],
-													  'short_wrong_candle' => [],
-													  'short_wrong_inverse_candle' => [],
+												/* these will be created as necessary
+												  'long_correct_candle' => [],
+												  'long_correct_inverse_candle' => [],
+												  'long_wrong_candle' => [],
+												  'long_wrong_inverse_candle' => [],
+												  'short_correct_candle' => [],
+												  'short_correct_inverse_candle' => [],
+												  'short_wrong_candle' => [],
+												  'short_wrong_inverse_candle' => [],
 
-													  'long_correct_trend' => [],
-													  'long_correct_inverse_trend' => [],
-													  'long_wrong_trend' => [],
-													  'long_wrong_inverse_trend' => [],
-													  'short_correct_trend' => [],
-													  'short_correct_inverse_trend' => [],
-													  'short_wrong_trend' => [],
-													  'short_wrong_inverse_trend' => [],
+												  'long_correct_trend' => [],
+												  'long_correct_inverse_trend' => [],
+												  'long_wrong_trend' => [],
+												  'long_wrong_inverse_trend' => [],
+												  'short_correct_trend' => [],
+												  'short_correct_inverse_trend' => [],
+												  'short_wrong_trend' => [],
+												  'short_wrong_inverse_trend' => [],
 
-													  '% WIN' => 0, */
+												  '% WIN' => 0, */
 											];
 										}
 
 										$prefix = $long ? 'long_' : 'short_';
 										$correct = $result['win'] ? 'correct_' : 'wrong_';
 
-										foreach ([/*'trend' => $trends[$instrument], */'candle' => isset($candles['current']) ? $candles['current'] : []] as $cot => $cots) {
+										foreach ([/* 'trend' => $trends[$instrument], */'candle' => isset($candles['current']) ? $candles['current'] : []] as $cot => $cots) {
 											foreach ($cots as $name => $value) {
 												$inverse = (($value > 0 && $long) || ($value < 0 && !$long)) ? '' : 'inverse_';
 												if (!isset($results[$bounds_strategy_name][$prefix . $correct . $cot][$name])) {
@@ -269,7 +256,7 @@ class EvaluateStrategiesCommand extends Command {
 					}
 				}
 			}
-			
+
 			if (!($min % 86400) || $min == $end_min) {
 				$percs = [];
 				foreach ($results as $strategy_name => $data) {
@@ -329,16 +316,14 @@ class EvaluateStrategiesCommand extends Command {
 
 					$percs[] = $perc;
 				}
-				
 				array_multisort($percs, $results);
-
 				$significant_results = [];
-				foreach($results as $result){
-					if($results[$strategy_name]['% PERCENTAGE WIN'] > 50) {
-						$significant_results[] = $result;
+				foreach ($results as $key => $result) {
+					if ($results[$strategy_name]['% PERCENTAGE WIN'] > 50) {
+						$significant_results[$key] = $result;
 					}
-				} 
-				if(empty($significant_results)) {
+				}
+				if (empty($significant_results)) {
 					$significant_results = $results;
 				}
 				file_put_contents('/home/terry/results/eurusd_MANY_IND_COMBINATIONs_any_candle_1and5mand15m_Intervals', "\n\n\nprinted:" . print_r($significant_results, 1) . ' ' . print_r('min so far ' . date('Y-m-d H:i:s', $min), 1) . "\n\n $instrument: Skipped $skipped due to incomplete data");
@@ -365,11 +350,13 @@ class EvaluateStrategiesCommand extends Command {
 	function unique_combinations($list) {
 		$size = count($list[0]);
 		$unique_list = [];
-		foreach($list as $items) {
-			if(count(array_unique($items)) == $size) {
+		foreach ($list as $items) {
+			if (count(array_unique($items)) == $size) {
 				$unique_list[] = $items;
 			}
 		}
 		return $unique_list;
 	}
+
 }
+
